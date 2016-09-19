3d5fb566493bfc1616d7e90708878982b55d385d
Vladimir <lenta-b@yandex.ru>
2016-09-19 16:46:32 +0300

    ДЗ6

---
+++ core/ToView.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: vladimirvahrusev
+ * Date: 16.09.16
+ * Time: 22:54
+ */
+class ToView
+{
+    public function p404()
+    {
+        include_once 'view/404.html';
+    }
+
+    public function pMain()
+    {
+        include_once 'view/main.tmpl';
+    }
+
+    public function pLogin ()
+    {
+        include_once 'view/login.html';
+    }
+
+    public function pRegister()
+    {
+        include_once 'view/register.html';
+    }
+}
+
+
+
